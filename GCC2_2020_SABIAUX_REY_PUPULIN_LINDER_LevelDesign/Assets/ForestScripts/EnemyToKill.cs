3fdbb2b652af87b2c985395f23c9334342af8760
unknown <fagipart@gmail.com>
2020-12-06 19:40:05 +0100

    Script EnemyToKill.cs down | SABIAUX Corentin GCC2

---
+++ GCC2_2020_SABIAUX_REY_PUPULIN_LINDER_LevelDesign/Assets/ForestScripts/EnemyToKill.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyToKill : MonoBehaviour
+{
+    //A placer sur un Gameobject vide
+    public GameObject[] enemies;
+    public GameObject pad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemies.Length == 2)
+        {
+            if (enemies[0] == null && enemies[1] == null)
+            {
+                pad.SetActive(true);
+            }
+        }
+
+        if (enemies.Length == 3)
+        {
+            if (enemies[0] == null && enemies[1] == null && enemies[2] == null)
+            {
+                pad.SetActive(true);
+            }
+        }
+
+        if (enemies.Length == 5)
+        {
+            if (enemies[0] == null && enemies[1] == null && enemies[2] == null && enemies[3] == null && enemies[4] == null)
+            {
+                pad.SetActive(true);
+            }
+        }
+    }
+}
