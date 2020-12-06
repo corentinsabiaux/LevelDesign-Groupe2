b84386739ed575d1bb5b9c6ef9b20804765cc588
unknown <fagipart@gmail.com>
2020-12-07 00:20:27 +0100

    Add MovingPlateformActivated.cs script | SABIAUX Corentin GCC2

---
+++ GCC2_2020_SABIAUX_REY_PUPULIN_LINDER_LevelDesign/Assets/ForestScripts/MovingPlateformActivated.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlateformActivated : MonoBehaviour
+{
+    public GameObject movingPlateform;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        movingPlateform.SetActive(true);
+    }
+}
